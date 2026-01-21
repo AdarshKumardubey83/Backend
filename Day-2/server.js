d06a5cfb4857621bafc1ea951458538f7a460c80
ADARSH KUMAR DUBEY <adarshkumardubey50@gmail.com>
2026-01-21 11:37:03 +0530

    Add Day-2 folder

---
+++ Day-2/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express(); // server instance create karna 
+
+app.get('/', (req,res) => {
+    res.send("Shree Harivansh")
+})
+
+app.get('/about', (req,res) => {
+    res.send("Shree harivansh,This is about page")
+})
+
+app.listen(3000); // server start karna 
