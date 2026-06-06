df7ac588b3c697af037e4d1677a9054261144f8c
ADARSH KUMAR DUBEY <adarshkumardubey50@gmail.com>
2026-06-06 10:21:15 +0530

    integration initialize

--- Day-14/Frontend/src/features/auth/pages/Login.jsx
+++ Day-14/Frontend/src/features/auth/pages/Login.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../style/form.scss'
+import {Link} from 'react-router'
+import axios from 'axios'
 
 const login = () => {
+
+  const [ username, setUsername ] = useState("")
+  const [ password, setPassword ] = useState("")
+
+  function handleSubmit(e){
+    e.preventDefault()
+    
+    axios.post("http://localhost:3000/api/auth/login",{
+      username,
+      password
+    },{withCredentials:true})
+    .then(res=>{
+      console.log(res.data);
+    })
+  }
+
+
   return (
     <main>
       <div className='form-container'>
         <h1>Login</h1>
-        <form>
-          <input type='text' name='username' placeholder='Enter username'></input>
-          <input type='password' name='password' placeholder='Enter password'></input>
+        <form onSubmit={handleSubmit}>
+          <input 
+          onInput={(e) => { setUsername(e.target.value) }}
+          type='text' 
+          name='username' 
+          placeholder='Enter username'>
+          </input>
+          <input 
+          onInput={(e) => { setPassword(e.target.value) }}
+          type='password' 
+          name='password' 
+          placeholder='Enter password'>
+          </input>
           <button type='submit'>Login</button>
         </form>
+        <p>Already have an account? <Link className='toggleAuthForm' to="/register">Register</Link></p>
       </div>
     </main>
   )
